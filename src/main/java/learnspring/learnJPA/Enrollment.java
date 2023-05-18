7cdc6802e8a5056bffd80c5c6245524be1817422
b116104@naver.com <b116104@naver.com>
2023-05-19 02:28:05 +0900

    learn: N:N 연관관계 학습

---
+++ src/main/java/learnspring/learnJPA/Enrollment.java
@@ -0,0 +1,21 @@
+package learnspring.learnJPA;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "enrollments")
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    // 생성자, getter, setter, 기타 메소드
+}
---
+++ src/main/java/learnspring/learnJPA/Student.java
@@ -0,0 +1,20 @@
+package learnspring.learnJPA;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Enrollment> enrollments = new ArrayList<>();
+
+    // 생성자, getter, setter, 기타 메소드
+}
