36509c78d78577aca60956ca7d74712384ad39d3
b116104@naver.com <b116104@naver.com>
2023-05-25 22:23:56 +0900

    test: ADD SERVICE TEST METHOD

--- src/main/java/learnspring/advanced/learnJPA/learnJPAService.java
+++ src/main/java/learnspring/advanced/learnJPA/learnJPAService.java
@@ -4,10 +4,23 @@ import learnspring.advanced.learnJPA.repository.StudentJPARepository;
 import learnspring.advanced.learnJPA.repository.TeacherJPARepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class learnJPAService {
     private final StudentJPARepository studentJPARepository;
     private final TeacherJPARepository teacherJPARepository;
+
+    @Transactional
+    public void saveTestData(){
+        List<String> teacherNameList = new ArrayList<>();
+        teacherNameList.add("jang");
+        teacherNameList.add("young");
+        Student student = new Student("YJ",teacherNameList);
+        studentJPARepository.save(student);
+    }
 }
