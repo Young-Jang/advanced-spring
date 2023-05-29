832caaa8525bd5a742625b5042c31de3c45d1753
b116104@naver.com <b116104@naver.com>
2023-05-29 16:13:08 +0900

    feat: learn teacher entity

--- src/main/java/learnspring/advanced/learnJPA/LearnJPAService.java
+++ src/main/java/learnspring/advanced/learnJPA/LearnJPAService.java
@@ -11,16 +11,24 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class learnJPAService {
+public class LearnJPAService {
     private final StudentJPARepository studentJPARepository;
     private final TeacherJPARepository teacherJPARepository;
 
     @Transactional
-    public void saveTestData(){
+    public Student saveStudent(){
         List<String> teacherNameList = new ArrayList<>();
         teacherNameList.add("jang");
         teacherNameList.add("young");
         Student student = new Student("YJ",teacherNameList);
-        studentJPARepository.save(student);
+        return studentJPARepository.save(student);
+    }
+
+    @Transactional
+    public Teacher saveTeacher(){
+        List<String> studentNameList = new ArrayList<>();
+        studentNameList.add("YJ");
+        Teacher teacher = new Teacher("jang",studentNameList);
+        return teacherJPARepository.save(teacher);
     }
 }
--- src/main/java/learnspring/advanced/learnJPA/Teacher.java
+++ src/main/java/learnspring/advanced/learnJPA/Teacher.java
@@ -1,16 +1,19 @@
 package learnspring.advanced.learnJPA;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "teachers")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +25,10 @@ public class Teacher {
     @JoinColumn(name = "teacher_id")
     private List<Enrollment> enrollments = new ArrayList<>();
 
+
+    public Teacher(String name, List<String> studentName){
+        this.name = name;
+        this.enrollments = studentName.stream().map(n->new Enrollment(n,name)).collect(Collectors.toList());
+    }
     // 생성자, getter, setter, 기타 메소드
 }
