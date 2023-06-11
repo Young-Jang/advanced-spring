67e84e3fcba2933032d85a06f6483cf28f1396ff
b116104@naver.com <b116104@naver.com>
2023-06-11 23:31:34 +0900

    feat: add passkit barcode format

---
+++ src/main/java/common/pkpass/passkit4j/model/BarcodeFormat.java
@@ -0,0 +1,42 @@
+package common.pkpass.passkit4j.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum BarcodeFormat {
+
+	PDF417("PKBarcodeFormatPDF417"),
+	QR("PKBarcodeFormatQR"),
+	AZTEC("PKBarcodeFormatAztec"),
+	CODE128("PKBarcodeFormatCode128");
+
+	private static final Map<String, BarcodeFormat> lookup;
+
+	static {
+		final Map<String, BarcodeFormat> _lookup = new HashMap<String, BarcodeFormat>();
+		for (BarcodeFormat entry : BarcodeFormat.values()) {
+			_lookup.put(entry.value(), entry);
+		}
+		lookup = Collections.unmodifiableMap(_lookup);
+	}
+
+	private final String value;
+
+	private BarcodeFormat(final String value) {
+		this.value = value;
+	}
+
+	@JsonValue
+	public String value() {
+		return this.value;
+	}
+
+	@JsonValue
+	public static BarcodeFormat forValue(String value) {
+		return lookup.get(value);
+	}
+
+}
